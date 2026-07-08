37bf984db924090ca80d21fb213f908fdc9d6839
celsofreschijr <celso.freschijr@gmail.com>
2026-07-08 09:20:13 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Configuração do projeto Firebase "Carteiras - CC"
+// Gerado a partir das chaves fornecidas pelo console do Firebase.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJhOx9ouNNxfJIOjLb3aAxhqctBf81Wso",
+  authDomain: "carteiras---cc.firebaseapp.com",
+  projectId: "carteiras---cc",
+  storageBucket: "carteiras---cc.firebasestorage.app",
+  messagingSenderId: "606315071442",
+  appId: "1:606315071442:web:d82382dd21d98e79b56a0b",
+  measurementId: "G-EH2JP23DF5"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
